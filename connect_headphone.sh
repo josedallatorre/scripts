ca769495ac97ea11e700c86229c5d6e25ad840e4
josedallatorre <jose.dallatorre@hotmai.com>
2021-01-29 00:43:10 +0100

    it work

--- connect_headphone.sh
+++ connect_headphone.sh
@@ -1,19 +1,9 @@
-coproc bluetoothctl
 headphoneMAC="1C:52:16:B9:23:A3"
-echo -e 'info $headphoneMAC\nexit' >&${COPROC[1]}
-
-echo -e 'info $headphoneMAC\nexit' >&${COPROC[1]}
-output=$(cat <&${COPROC[0]})
-echo $output
-
-#bluetoothctl --power on
-#bluetoothctl --agent on
-#bluetoothctl --default-agent
-#bluetoothctl --remove $headphoneMAC
-#sleep 1s
-#bluetoothctl --trust $headphoneMAC
-#sleep 1s
-#bluetoothctl --pair $headphoneMAC
-#sleep 5s
-#bluetoothctl --connect $headphoneMAC
-#bluetoothctl --exit
+echo -e "remove $headphoneMAC\n" | bluetoothctl 
+sleep 5s
+echo -e "trust $headphoneMAC\n" | bluetoothctl
+sleep 5s
+echo -e "pair $headphoneMAC\n" | bluetoothctl
+sleep 5s
+echo -e "connect $headphoneMAC\n" | bluetoothctl
+sleep 5s
